6ea01d79278a604228d8312bff1718206a1ebe19
Efe <efesezen1@outlook.com>
2024-11-11 13:01:43 +0300

    Play icon renders conditionally on track index and playing status.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Flex, Box, Slider } from '@radix-ui/themes'
 import {
    LoopIcon,
@@ -17,81 +17,44 @@ import QueueIcon from './icon/QueueIcon'
 import MicrophoneIcon from './icon/MicrophoneIcon'
 import DevicesIcon from './icon/DevicesIcon'
 
-const Player = () => {
+const Player = ({ previewUrl }) => {
    const [volume, setVolume] = useState(50)
    const [isPlaying, setIsPlaying] = useState(false)
    const [isOnLoop, setIsOnLoop] = useState(false)
    const [isShuffled, setIsShuffled] = useState(false)
-   const [spotifyPlayer, setSpotifyPlayer] = useState(null)
+   const audioRef = useRef(null)
 
-   useEffect(() => {
-      let player
-      window.onSpotifyWebPlaybackSDKReady = () => {
-         const token = import.meta.env.VITE_APP_PLAYER_TOKEN
-         player = new Spotify.Player({
-            name: 'Web Playback SDK Quick Start Player',
-            getOAuthToken: (cb) => {
-               cb(token)
-            },
-            volume: volume / 100,
-         })
-         setSpotifyPlayer(player)
+   // Toggle play/pause
+   const togglePlayPause = () => {
+      if (isPlaying) {
+         audioRef.current.pause()
+      } else {
+         audioRef.current.play()
       }
-   }, [])
-
-   const playerManagement = async () => {
-      try {
-         console.log(spotifyPlayer)
-         await spotifyPlayer.connect()
-         console.log(
-            'The Web Playback SDK successfully connected to Spotify! 😇'
-         )
-
-         console.log(spotifyPlayer)
-
-         spotifyPlayer.addListener('player_state_changed', (state) => {
-            console.log('state changed')
-            if (!state) {
-               return
-            }
-            console.log('track:')
-            console.log(state.track_window.current_track)
-
-            spotifyPlayer.getCurrentState().then((state) => {
-               // !state ? setActive(false) : setActive(true)
-               console.log(state)
-            })
-         })
-      } catch (err) {
-         console.error(err)
-      }
+      setIsPlaying(!isPlaying)
    }
 
+   // Handle volume changes
    useEffect(() => {
-      if (!spotifyPlayer) return
-
-      playerManagement()
-
-      return () => {
-         spotifyPlayer.disconnect()
+      if (audioRef.current) {
+         audioRef.current.volume = volume / 100
       }
-   }, [spotifyPlayer])
+   }, [volume])
 
    return (
-      <Flex direction="row" className="w-full  ">
+      <Flex direction="row" className="w-full">
+         <audio ref={audioRef} src={previewUrl} loop={isOnLoop} />
+
          <Flex className="w-1/4 lg:w-1/6 bg-slate-50 mb-2">efe</Flex>
+
          <Flex
             direction="row"
             justify="center"
             align="center"
-            className="w-2/4 lg:w-4/6 "
-            // className="w-full bg-yellow-50"
+            className="w-2/4 lg:w-4/6"
          >
-            <Flex className=" w-7/12 mb-2" direction="column">
+            <Flex className="w-7/12 mb-2" direction="column">
                <Flex className="w-full flex-row justify-evenly p-3">
-                  {/* <Box className="hover:bg-red-300 active:bg-red-500 rounded-full p-2 hover:text-white active:text-white transition-all duration-300">
-                     <ShuffleIcon onClick={() => setIsShuffled(!isShuffled)} />
-                  </Box> */}
                   <PlayerBox
                      children={
                         <ShuffleIcon
@@ -102,15 +65,11 @@ const Player = () => {
                   <PlayerBox children={<TrackPreviousIcon />} />
                   {isPlaying ? (
                      <PlayerBox
-                        children={
-                           <PauseIcon onClick={() => setIsPlaying(false)} />
-                        }
+                        children={<PauseIcon onClick={togglePlayPause} />}
                      />
                   ) : (
                      <PlayerBox
-                        children={
-                           <PlayIcon onClick={() => setIsPlaying(true)} />
-                        }
+                        children={<PlayIcon onClick={togglePlayPause} />}
                      />
                   )}
                   <PlayerBox children={<TrackNextIcon />} />
@@ -121,15 +80,17 @@ const Player = () => {
                   />
                </Flex>
                <Slider
-                  defaultValue={[volume]}
+                  value={[volume]}
+                  onValueChange={(value) => setVolume(value[0])}
                   variant="soft"
                   highContrast
                   size="3"
                />
             </Flex>
          </Flex>
+
          <Flex
-            className=" w-1/4 lg:w-1/6  mb-2"
+            className="w-1/4 lg:w-1/6 mb-2"
             align="center"
             justify="center"
             direction="column"
@@ -142,7 +103,8 @@ const Player = () => {
                <Flex className="w-4/12" direction="row" align="center">
                   <SpeakerModerateIcon />
                   <Slider
-                     defaultValue={[volume]}
+                     value={[volume]}
+                     onValueChange={(value) => setVolume(value[0])}
                      variant="soft"
                      highContrast
                      size="1"
@@ -154,10 +116,11 @@ const Player = () => {
       </Flex>
    )
 }
+
 const PlayerBox = ({ children, className }) => {
    return (
       <Box
-         className={`hover:bg-red-300 active:bg-red-500 rounded-full p-2  transition-all duration-300 hover:text-white active:text-white ${className}`}
+         className={`hover:bg-red-300 active:bg-red-500 rounded-full p-2 transition-all duration-300 hover:text-white active:text-white ${className}`}
       >
          {children}
       </Box>
